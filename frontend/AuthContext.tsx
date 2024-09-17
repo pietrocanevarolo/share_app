9f12f0a92a930c6a17a7ae6753bf9841b248d1ce
pietrocanevarolo <122265394+pietrocanevarolo@users.noreply.github.com>
2024-09-17 14:55:11 +0200

    Update AuthContext.tsx

--- frontend/AuthContext.tsx
+++ frontend/AuthContext.tsx
@@ -28,7 +28,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (context === undefined) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error('useAuth must be used within an AuthProviderr');
   }
   return context;
 };
